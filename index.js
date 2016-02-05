fc5d0fda50b598586edc352c0a3e0b4c69c3e4c1
Nick Vladimiroff <nickvladimiroff@gmail.com>
2016-02-05 13:33:54 -0500

    Change port to use env variable.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ let app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000/");
+app.listen(process.env.PORT, () => {
+  console.log("http://localhost:" + process.env.PORT + "/");
 });
