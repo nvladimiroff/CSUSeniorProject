d42b7711016a56fb4863d587b2f4c0bb783ca1e8
Nick Vladimiroff <nickvladimiroff@gmail.com>
2016-02-09 12:21:26 -0500

    Added the test view I showed you guys on Monday.

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 let express = require('express');
+let mustacheExpress = require('mustache-express');
+let session = require('express-session');
 
 let app = express();
 
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+
 app.use(express.static('public'));
+
+app.use(session({
+  secret: 'hunter2',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.get('/test', (req, res) => {
+  if(req.session.views) {
+    req.session.views++;
+  } else {
+    req.session.views = 1;
+  }
+
+  res.render('test', { name: "Nick Vladimiroff", classname: "CIS 484", views: req.session.views });
+});
 
 app.listen(process.env.PORT, () => {
   console.log("http://localhost:" + process.env.PORT + "/");
