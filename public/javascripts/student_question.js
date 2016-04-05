bad92f5e00bc37c65dad660359cb04d88c8f034d
Nick Vladimiroff <nickvladimiroff@gmail.com>
2016-04-05 13:12:17 -0400

    Selecting answers works with some ok animations too.

--- public/javascripts/student_question.js
+++ public/javascripts/student_question.js
@@ -1,13 +1,16 @@
-var Answer = props => (
-  <div className="answer-block" id="answer-{this.props.letter}">
-    <span className="letter">{props.letter}</span>
-    <span className="answer">{props.answer}</span>
-  </div>
-);
-
 var Finished = props => (
   <div className="finished">
     <h1>The test is finished!</h1>
+  </div>
+);
+
+var Answer = props => (
+  <div
+    onClick={props.onClick}
+    className={"answer-block " + (props.selected ? "answer-block-selected" : "")}
+    id={"answer-" + props.letter}>
+    <span className={"letter " + (props.selected ? "letter-selected" : "")}>{props.letter}</span>
+    <span className="answer"> {props.answer}</span>
   </div>
 );
 
@@ -40,14 +43,14 @@ var StudentApp = React.createClass({
   },
 
   componentDidMount: function() {
-    setInterval(() => {
+    /* setInterval(() => {
       if(this.state.questionNum+1 < this.state.questions.length) {
         this.setState({ questionNum: this.state.questionNum+1});
       } else {
         this.setState({ finished: true });
       }
     }, this.state.timelimit*1000);
-  },
+    */  },
 
   render: function() {
     if(!this.state.finished) {
@@ -67,8 +70,9 @@ var StudentApp = React.createClass({
 
 var QuestionBlock = React.createClass({
   getInitialState: function() {
-    return {question: "", answers: ["?","?","?","?"]}
+    return {question: "", answers: ["?","?","?","?"], selectedAnswer: "X"}
   },
+
   render: function() {
     return (
       <div className="container">
@@ -77,17 +81,39 @@ var QuestionBlock = React.createClass({
         </div>
         <div className="answers">
           <div className="row">
-            <Answer letter="A" answer={this.props.answers[0]}/>
-            <Answer letter="B" answer={this.props.answers[1]}/>
+            <Answer
+              onClick={this.click("A").bind(this)}
+              selected={this.state.selectedAnswer == "A"}
+              letter="A"
+              answer={this.props.answers[0]}/>
+            <Answer
+              onClick={this.click("B").bind(this)}
+              selected={this.state.selectedAnswer == "B"}
+              letter="B"
+              answer={this.props.answers[1]}/>
           </div>
           <div className="row">
-            <Answer letter="C" answer={this.props.answers[2]}/>
-            <Answer letter="D" answer={this.props.answers[3]}/>
+            <Answer
+              onClick={this.click("C").bind(this)}
+              selected={this.state.selectedAnswer == "C"}
+              letter="C"
+              answer={this.props.answers[2]}/>
+            <Answer
+              onClick={this.click("D").bind(this)}
+              selected={this.state.selectedAnswer == "D"}
+              letter="D"
+              answer={this.props.answers[3]}/>
           </div>
         </div>
       </div>
     );
   },
+
+  click: function(letter) {
+    return function() {
+      this.setState({ selectedAnswer: letter });
+    };
+  }
 });
 
 ReactDOM.render(
